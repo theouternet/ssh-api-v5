13639c29badc414cf4305bc5f7a7e7cf432809c6
theouternet <jrougeau@gmail.com>
2020-07-30 13:42:34 -0500

    routes2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
       resources :softwares, only: [:index, :show]
       resources :categories, only: [:index, :show]
 
